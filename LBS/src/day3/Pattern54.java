dc829ef9b7e3933f75e874be37c4659f0bc59de9
RishiRaj77 <rishirajdubey72@gmail.com>
2025-05-31 20:35:19 +0530

    patterns

--- LBS/src/day3/Pattern54.java
+++ LBS/src/day3/Pattern54.java
@@ -1,13 +1,13 @@
 package day3;
 
-public class Pattern53 {
+public class Pattern54 {
 	 public static void main(String[] args) {
 	        for (char i = 'E'; i >= 'A'; i--) {
 	            for (char j = 'A'; j <= 'E'; j++) {
 	                if (j >= i) {
 	                    if (i == 'A' || i == j || j == 'E') {
 	                        // Calculate number as distance from i + 1
-	                        System.out.print(i);
+	                        System.out.print(j);
 	                    } else {
 	                        System.out.print(" ");  // spaces for hollow parts
 	                    }
