0ecfc1931869f361fc4421bbe78f5812c992f809
RishiRaj77 <rishirajdubey72@gmail.com>
2025-06-02 16:43:45 +0530

    striver

---
+++ LBS/src/atoz/Pattern6.java
@@ -0,0 +1,19 @@
+package atoz;
+
+public class Pattern6 {
+	public static void main(String[] args) {
+		for (int i = 0; i <5; i++) {
+			for (int j = 0; j< (4-i+1); j++) { // n-row+1
+				
+					System.out.print(" * ");
+				
+	
+				
+			}System.out.println("  ");
+			
+		} 
+	
+	} 
+
+
+}
---
+++ LBS/src/atoz/Patterns.java
@@ -0,0 +1,18 @@
+package atoz;
+
+import java.util.Iterator;
+
+public class Patterns {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.print(" * ");
+				
+			}System.out.println("");
+			
+		} 
+	
+	} 
+	
+
+}
